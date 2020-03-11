d41e3541f420aa4012d45e85adc2e3f6b0cddbda
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-11 19:35:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,8 @@ def dennis_ritchies_language
         }
      }
 
-		lang = programmer_hash [:dennis_ritchie][:languages][0]
-		lang
+		programmer_hash [:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
