c685940c8014a3e5ce0383967767c45e5d79a31f
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-11 19:30:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 
-		lang = programmer_hash[:dennis_ritchie][.to_s :languages]
+		lang = programmer_hash.to_s [:dennis_ritchie][:languages]
 		lang
 end
 
